9c495674d4d61f1d287fcfdbe66bf3143e500417
Melker Öberg <memleoberg@gmail.com>
2023-10-07 01:01:17 +0200

    js

--- blackJack/main.js
+++ blackJack/main.js
@@ -1,15 +1,25 @@
-count = []
+let count = [] 
 
-let deck  = {
-    heart: count,
-    diamond: count,
-    clubs:  count,
-    spades:  count,
-    
+let deck = {
+    heart: [count],
+    diamond: [count],
+    clubs: [count],
+    spades: [count],
 }
 
-for (let i = 0; i < 14; i++) {
-    count.push(i)    
+for (let i = 1; i <= 13; i++) {
+    count.push(i);
 }
-// [random index]
-console.log(deck.heart[13])
+
+
+// how do i pick a random value ?
+let randomSuit = Object.keys(deck)[Math.floor(Math.random() * 4)];
+let randomCardIndex = Math.floor(Math.random() * deck[randomSuit].length);
+let randomCard = deck[randomSuit][randomCardIndex];
+console.log(`${randomCard} of ${randomSuit}`);
+
+// How to pick a random key from the deck
+let suits = Object.keys(deck);
+let randomSuitIndex = Math.floor(Math.random() * suits.length);
+let randomSuitKey = suits[randomSuitIndex];
+console.log(randomSuitKey)
