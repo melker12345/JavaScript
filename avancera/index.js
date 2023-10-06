02604677f12f07e010b878c771d5e5e717f299d0
Melker Öberg <memleoberg@gmail.com>
2023-10-06 02:39:58 +0200

    js

--- avancera/index.js
+++ avancera/index.js
@@ -1,6 +1,20 @@
-function sum(a){
-    return a.reduce((total, current) => total + current, 0);
-}
+// function sum(a){
+//     return a.reduce((total, current) => total + current, 0);
+// }
 
-console.log(sum([1, 2, 3]))
-console.log(sum([1, 2, 3, 4]))  
+// console.log(sum([1, 2, 3]))
+// console.log(sum([1, 2, 3, 4]))  
+
+// function average(a){
+//     return  isNaN(a) ? (a.reduce((total, current) => total + current, 0) / a.length): null;
+// }
+
+
+function reverse(a) {
+    b = []
+    // set i = a.length - 1 because a.length counts a[0] as 1  
+    for (let i = a.length - 1; i >= 0; i--) {
+        b.push(a[i])
+    }
+}
+console.log(reverse([1, 2, 3]))
