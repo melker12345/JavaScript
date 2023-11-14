9c6d8ac566fe2cbc7b673583c2d5d689d6bdb874
melker12345 <memleoberg@gmail.com>
2023-11-14 23:35:26 +0100

    :)

--- project2/main.js
+++ project2/main.js
@@ -1,49 +1,51 @@
 let rawData = {}; // Variable to store raw JSON data
-const apiUrl = "https://reqres.in/api/users";
+const apiUrl = "https://reqres.in/api/users/";
 let isJsonView = false; // Flag to toggle view
 
 function fetchUsers() {
     fetch(apiUrl)
-        .then(response => {
+        .then((response) => {
             if (!response.ok) {
                 throw new Error(`HTTP error! Status: ${response.status}`);
             }
             return response.json();
         })
-        .then(data => {
+        .then((data) => {
             rawData = data; // Store raw data
             renderUsers(); // Render users
         })
-        .catch(error => console.error('Fetch error:', error));
+        .catch((error) => console.error("Fetch error:", error));
 }
 
 function renderUsers() {
-    const usersDiv = document.getElementById('posts');
-    usersDiv.innerHTML = ''; // Clear existing content
-    rawData.data.forEach(user => {
+    const usersDiv = document.getElementById("posts");
+    usersDiv.innerHTML = ""; // Clear existing content
+    rawData.data.forEach((user) => {
         usersDiv.innerHTML += createUserHTML(user); // Add each user
     });
-    renderUserChart()
+
+    // Update localStorage with the current state of rawData
+    localStorage.setItem("rawData", JSON.stringify(rawData));
 }
 
 function toggleView() {
     isJsonView = !isJsonView;
-    document.getElementById('posts').style.display = isJsonView ? 'none' : 'block';
-    document.getElementById('jsonView').style.display = isJsonView ? 'block' : 'none';
+    document.getElementById("posts").style.display = isJsonView ? "none" : "block";
+    document.getElementById("jsonView").style.display = isJsonView ? "block" : "none";
     if (isJsonView) {
-        document.getElementById('jsonView').textContent = JSON.stringify(rawData, null, 2);
+        document.getElementById("jsonView").textContent = JSON.stringify(rawData, null, 2);
     }
 }
 
 function createUserHTML(user) {
     return `
-        <div id="user-${user.id}">
-            <h4>${user.first_name} ${user.last_name}</h4>
-            <p>${user.email}</p>
-            <button aria-label="Update user" onclick="updateUser(${user.id})">Update</button>
-            <button aria-label="Delete user" onclick="deleteUser(${user.id})">Delete</button>
-        </div>
-    `;
+                <div id="user-${user.id}">
+                    <h4>${user.first_name} ${user.last_name}</h4>
+                    <p>${user.email}</p>
+                    <button aria-label="Update user" onclick="updateUser(${user.id})">Update</button>
+                    <button aria-label="Delete user" onclick="deleteUser(${user.id})">Delete</button>
+                </div>
+            `;
 }
 
 function createUser() {
@@ -51,25 +53,36 @@ function createUser() {
     const lastName = document.getElementById("userLastName").value;
     const email = document.getElementById("userEmail").value;
 
-
     fetch(apiUrl, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ first_name: name, last_name: lastName, email: email })
+        body: JSON.stringify({
+            first_name: name,
+            last_name: lastName,
+            email: email,
+        }),
     })
-        .then(response => response.json())
-        .then(user => {
+        .then((response) => response.json())
+        .then((user) => {
             console.log("Created user:", user);
             // Add the new user to rawData.data
-            rawData.data.push({ ...user, id: rawData.data.length + 1, first_name: name, last_name: lastName, email: email });
+            rawData.data.push({
+                // ...user expands the user object
+                ...user,
+                id: rawData.data.length + 1,
+                first_name: name,
+                last_name: lastName,
+                email: email,
+            });
 
             // Update the views without re-fetching all users
             renderUsers();
             if (isJsonView) {
-                document.getElementById('jsonView').textContent = JSON.stringify(rawData, null, 2);
+                document.getElementById("jsonView").textContent =
+                    JSON.stringify(rawData, null, 2);
             }
         })
-        .catch(error => console.error('Error creating user:', error));
+        .catch((error) => console.error("Error creating user:", error));
 }
 
 function updateUser(id) {
@@ -78,43 +91,55 @@ function updateUser(id) {
     const newEmail = document.getElementById("userEmail").value;
 
     fetch(`${apiUrl}/${id}`, {
-        method: "PUT",
         headers: {
             "Content-Type": "application/json",
         },
-        body: JSON.stringify({ first_name: newName, last_name: newLastName, email: newEmail }),
+        body: JSON.stringify({
+            first_name: newName,
+            last_name: newLastName,
+            email: newEmail,
+        }),
     })
-        .then(response => response.json())
-        .then(user => {
+        .then((response) => response.json())
+        .then((user) => {
             console.log("Updated user:", user);
             const userDiv = document.getElementById(`user-${id}`);
-            userDiv.innerHTML = createUserHTML({ ...user, first_name: newName, last_name: newLastName, email: newEmail });
+            userDiv.innerHTML = createUserHTML({
+                ...user,
+                first_name: newName,
+                last_name: newLastName,
+                email: newEmail,
+            });
         });
+        renderUsers();
 }
 
 function deleteUser(userId) {
-    fetch(apiUrl + '/' + userId, { method: 'DELETE' })
-        .then(response => {
+    fetch(`${apiUrl}/${userId}`, { method: "DELETE" })
+        .then((response) => {
             if (response.ok) {
-                console.log('User deleted successfully');
+                console.log("User deleted successfully");
 
-                const numericId = parseInt(userId, 10);
-                rawData.data = rawData.data.filter(user => user.id !== numericId);
+                const numericId = + userId; // Convert to number to compare with id in rawData
+                rawData.data = rawData.data.filter(
+                    (user) => user.id !== numericId
+                ); // this code filters out the user with the given id from the rawData array of users and returns a new array without that user 
 
                 const userDiv = document.getElementById(`user-${userId}`);
                 if (userDiv) {
-                    userDiv.parentNode.removeChild(userDiv);
+                    userDiv.remove();
                 }
 
                 if (isJsonView) {
-                    document.getElementById('jsonView').textContent = JSON.stringify(rawData, null, 2);
-                }
+                    document.getElementById("jsonView").textContent =
+                        JSON.stringify(rawData, null, 2);
+                } // Update the view of the JSON data
+                renderUsers(); // render users an update the view of the users
             } else {
-                console.error('Deletion failed');
+                console.error("Deletion failed");
             }
         })
-        .catch(error => console.error('Error:', error));
+        .catch((error) => console.error("Error:", error));
 }
 
-fetchUsers();   
-
+fetchUsers();
